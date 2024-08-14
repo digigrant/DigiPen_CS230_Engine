b39792d65df2eb532b49310ba8ff4db3bc354163
digigrant <digigrant@users.noreply.github.com>
2024-08-14 00:00:28 -0700

    Initial build

---
+++ CS230 Project3/Source/Matrix2D.c
@@ -0,0 +1,93 @@
+//------------------------------------------------------------------------------
+//
+// File Name:	Matrix2D.c
+// Author(s):	Grant Joyner (g.joyner)
+// Project:		Project 0
+// Course:		CS230S24
+//
+// Copyright © 2024 DigiPen (USA) Corporation.
+//
+//------------------------------------------------------------------------------
+
+#include "stdafx.h"
+#include "Matrix2D.h"
+
+//------------------------------------------------------------------------------
+// Private Constants:
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+// Private Structures:
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+// Public Variables:
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+// Private Variables:
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+// Private Function Declarations:
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+// Public Functions:
+//------------------------------------------------------------------------------
+
+void Matrix2DIdentity(Matrix2D* pResult)
+{
+	UNREFERENCED_PARAMETER(pResult);
+}
+
+void Matrix2DTranspose(Matrix2D* pResult, const Matrix2D* pMtx)
+{
+	UNREFERENCED_PARAMETER(pResult);
+	UNREFERENCED_PARAMETER(pMtx);
+}
+
+void Matrix2DConcat(Matrix2D* pResult, const Matrix2D* pMtx0, const Matrix2D* pMtx1)
+{
+	UNREFERENCED_PARAMETER(pResult);
+	UNREFERENCED_PARAMETER(pMtx0);
+	UNREFERENCED_PARAMETER(pMtx1);
+}
+
+void Matrix2DTranslate(Matrix2D* pResult, float x, float y)
+{
+	UNREFERENCED_PARAMETER(pResult);
+	UNREFERENCED_PARAMETER(x);
+	UNREFERENCED_PARAMETER(y);
+}
+
+void Matrix2DScale(Matrix2D* pResult, float x, float y)
+{
+	UNREFERENCED_PARAMETER(pResult);
+	UNREFERENCED_PARAMETER(x);
+	UNREFERENCED_PARAMETER(y);
+}
+
+void Matrix2DRotDeg(Matrix2D* pResult, float angle)
+{
+	UNREFERENCED_PARAMETER(pResult);
+	UNREFERENCED_PARAMETER(angle);
+}
+
+void Matrix2DRotRad(Matrix2D* pResult, float angle)
+{
+	UNREFERENCED_PARAMETER(pResult);
+	UNREFERENCED_PARAMETER(angle);
+}
+
+void Matrix2DMultVec(Vector2D* pResult, const Matrix2D* pMtx, const Vector2D* pVec)
+{
+	UNREFERENCED_PARAMETER(pResult);
+	UNREFERENCED_PARAMETER(pMtx);
+	UNREFERENCED_PARAMETER(pVec);
+}
+
+//------------------------------------------------------------------------------
+// Private Functions:
+//------------------------------------------------------------------------------
+
